41857e754349abc93a9ed192859535d66c90db68
anthony.phan94 <anthony.phan94@outlook.com>
2025-12-30 15:42:37 -0600

    Fix port 8080 và trigger deploy

--- backend/main.py
+++ backend/main.py
@@ -339,6 +339,6 @@ if os.path.isdir(STATIC_DIR):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
 
